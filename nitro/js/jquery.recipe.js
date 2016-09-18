41dbe3ba9555a04099232ea7773a244633abc587
Seung Won [Tom] Lee <leesw@d207-023-175-128.wireless.sfu.ca>
2016-09-18 00:23:12 -0700

    Recipe update

--- nitro/js/jquery.recipe.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function() {
-  var jsonURL = "https://spreadsheets.google.com/feeds/list/1eUdfXnxuwqZPScarg9bxeNDhmK7Pbjex_1JB4yPeFAk/1/public/values?alt=json-in-script&callback=json";
-  $.getJSON( jsonURL, function( data ) {
-    console.log(data);
-});
-});
